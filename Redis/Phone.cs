123d957be84f3f7d1b409025b9ccd8972b6cb78e
Jin Chen <chenjin10@gmail.com>
2016-08-25 17:12:03 +1200

    Initial commit.

---
+++ Redis/Phone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redis
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public Person Owner { get; set; }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Profession { get; set; }
+    }
+}
---
+++ Redis/Program.cs
@@ -0,0 +1,54 @@
+﻿using ServiceStack.Redis;
+using ServiceStack.Redis.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Redis
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "localhost";
+            int port = 6379;
+            string elementKey = "testKeyRedis";
+            using (RedisClient redisClient = new RedisClient(host, port))
+            {
+                if(redisClient.Get<string>(elementKey)==null)
+                {
+                    redisClient.Set(elementKey, "some cached value");
+                }
+
+                Console.WriteLine("Item value is: {0}", redisClient.Get<string>(elementKey));
+
+                IRedisTypedClient<Phone> phones = redisClient.As<Phone>();
+                Phone phoneFive = phones.GetValue("5");
+                if (phoneFive == null)
+                {
+                    phoneFive = new Redis.Phone
+                    {
+                        Id = 5,
+                        Manufacturer = "Apple",
+                        Model = "xxxx",
+                        Owner = new Person
+                        {
+                            Id = 1,
+                            Age = 90,
+                            Name = "OldOne",
+                            Profession = "teacher",
+                            Surname = "SurName"
+                        }
+                    };
+                }
+                phones.SetValue(phoneFive.Id.ToString(), phoneFive);
+                Console.WriteLine("Phone model is: {0},  Phone Owner Name is: {1}", phoneFive.Manufacturer, phoneFive.Owner.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
