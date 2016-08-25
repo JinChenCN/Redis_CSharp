4762ecf931d2645e43c5e5e564df095a14a1af26
Jin Chen <chenjin10@gmail.com>
2016-08-25 17:43:10 +1200

    Tested Redis Lists.

--- Redis/Program.cs
+++ Redis/Program.cs
@@ -46,6 +46,72 @@ namespace Redis
                 }
                 phones.SetValue(phoneFive.Id.ToString(), phoneFive);
                 Console.WriteLine("Phone model is: {0},  Phone Owner Name is: {1}", phoneFive.Manufacturer, phoneFive.Owner.Name);
+
+                IRedisList<Phone> mostSelling = phones.Lists["urn:phones:mostselling"];
+                IRedisList<Phone> oldCollection = phones.Lists["urn:phones:oldCollection"];
+
+                Person phoneOwner = new Person
+                {
+                    Id = 7,
+                    Age = 90,
+                    Name = "OldOne",
+                    Profession = "teacher",
+                    Surname = "SurName"
+                };
+
+                mostSelling.Add(new Phone
+                {
+                    Id = 5,
+                    Manufacturer = "Apple",
+                    Model = "54321",
+                    Owner = phoneOwner
+                });
+
+                oldCollection.Add(new Phone
+                {
+                    Id = 8,
+                    Manufacturer = "Moto",
+                    Model = "111111",
+                    Owner = phoneOwner
+                });
+
+                var upgradedPhone = new Phone
+                {
+                    Id = 5,
+                    Manufacturer = "LG",
+                    Model = "12345",
+                    Owner = phoneOwner
+                };
+                
+                mostSelling.Add(upgradedPhone);
+                Console.WriteLine("Phones in mostSelling list:");
+                foreach (Phone ph in mostSelling)
+                {
+                    Console.WriteLine(ph.Id);
+                }
+
+                Console.WriteLine("Phones in oldCollection list:");
+                foreach (Phone ph in oldCollection)
+                {
+                    Console.WriteLine(ph.Id);
+                }
+                oldCollection.Remove(upgradedPhone);
+                IEnumerable<Phone> LGPhones = mostSelling.Where(ph => ph.Manufacturer == "LG");
+                foreach (Phone ph in LGPhones)
+                {
+                    Console.WriteLine("LG phone Id: {0}, LG phone Model: {1}", ph.Id, ph.Model);
+                }
+
+                Phone singleElement = oldCollection.FirstOrDefault(ph => ph.Id == 8);
+                Console.WriteLine("singleElement phone Id: {0}, singleElement phone Model: {1}", singleElement.Id, singleElement.Model);
+
+                phones.SetSequence(0);
+
+                redisClient.Remove("urn:phones:mostselling");
+                redisClient.Remove("urn:phones:oldCollection");
+
+                Console.WriteLine("urn:phones:mostselling Count: {0}", phones.Lists["urn:phones:mostselling"].Count);
+
             }
 
             Console.ReadKey();
